be1ff791fa5ef68b4ee4b993f6d130b2296e868e
lipton-web <cpt.lipton@gmail.com>
2022-02-15 16:40:25 +0900

    ts test

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 // type Type = string | number;
-var name = "kim";
-var age = 32;
+var 이름 = "kim";
+var 나이 = 50;
 var marryed = false;
+var 회원들 = ['kim', 'park'];
+var 회원멤버 = { member1: "kim", member2: "park" };
+var project = {
+    member: ['kim', 'park'],
+    days: 30,
+    started: true,
+};
--- index.ts
+++ index.ts
@@ -1,5 +1,18 @@
 // type Type = string | number;
 
-let name: string = "kim";
-let age: number = 32;
+let 이름 = "kim";
+let 나이: number = 50;
 let marryed: boolean = false;
+
+let 회원들: string[] = ['kim', 'park']
+let 회원멤버 :{member1: string, member2:string} = {member1: "kim", member2: "park"}
+
+let project: {
+	member : string[],
+	days : number,
+	started : boolean,
+} = {
+  member : ['kim', 'park'],
+  days : 30,
+  started : true,
+}
